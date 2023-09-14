c4c56fbe3c2543776727a682850ba7661e13622e
pinkfloydfan <jxchen98@gmail.com>
2023-09-14 19:50:37 +0100

    rename project directory and get template working

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Call Finnhub API, do thins with it
 
 # Requirements
+Install requirements by: 
+`pip install -r requirements.txt `
--- src/finnhub_client/settings.py
+++ src/finnhub_client/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'finnhub_client.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'finnhub_client/templates/',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
